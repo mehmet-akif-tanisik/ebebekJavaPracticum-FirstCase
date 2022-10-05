f1e6d2f26b5c5d0093d6a0afab2b38d64388b669
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-10-05 22:09:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee mehmet = new Employee("Mehmet",900,40,2018);
+        Employee ahmet = new Employee("Ahmet",1000,41,2018);
+        Employee halil = new Employee("Halil",1000,40,2012);
+        Employee deniz = new Employee("Deniz",1000,40,2010);
+        Employee eren = new Employee("Eren",1000,40,2000);
+
+        System.out.println("IF TOTAL WORK HOURS LESS OR EQUAL TO 40 HOURS ");
+        System.out.println("AND SALARY IS LESS THAN 1000 , SEE TAX AND BONUS\n");
+        System.out.println(mehmet);
+        System.out.println("----------------------------------------------------");
+        System.out.println("IF TOTAL WORK HOURS GREATER THAN 40 HOURS ");
+        System.out.println("AND SALARY IS GREATER THAN 1000 , SEE TAX AND BONUS\n");
+        System.out.println(ahmet);
+        System.out.println("----------------------------------------------------");
+        System.out.println("IF TOTAL WORKING YEARS LESS THAN 10 YEARS\n");
+        System.out.println(halil);
+        System.out.println("----------------------------------------------------");
+        System.out.println("IF TOTAL WORKING YEARS GREATER THAN 10 YEARS AND LESS THAN 20 YEARS\n");
+        System.out.println(deniz);
+        System.out.println("----------------------------------------------------");
+        System.out.println("IF TOTAL WORKING YEARS GREATER THAN 20 YEARS\n");
+        System.out.println(eren);
+
+
+
+
+
+
+
+
+
+    }
+}
